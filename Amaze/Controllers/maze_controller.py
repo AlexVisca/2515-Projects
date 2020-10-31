a60da900165dc49091922dca61352e111f10c23c
Alex Visca <47060218+AlexVisca@users.noreply.github.com>
2020-10-31 14:45:24 -0700

    Created Maze controller

--- Amaze/Controllers/maze_controller.py
+++ Amaze/Controllers/maze_controller.py
@@ -1,2 +1,22 @@
 class MazeController:
     pass
+
+
+if __name__ == '__main__':
+    running = True
+    while running:
+        if input not in ['a', 's', 'w', 'd']:
+            print("Use WASD to move player.")
+        elif input == 'a':
+            pass
+        elif input == 's':
+            pass
+        elif input == 'w':
+            pass
+        elif input == 'd':
+            pass
+        elif input == 'q':
+            raise SystemExit
+
+
+
