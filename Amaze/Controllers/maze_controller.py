da9c5a04eb6491efeed27a9a683a680825449531
apu-pixel <62687887+apu-pixel@users.noreply.github.com>
2020-11-01 10:50:58 -0800

    Update maze_controller.py

--- Amaze/Controllers/maze_controller.py
+++ Amaze/Controllers/maze_controller.py
@@ -6,43 +6,74 @@ from mazegame import Player
 from mazegame import App
 
 class MazeController:
+    windowWidth = 800
+    windowHeight = 600
+    player = 0
+ 
     def __init__(self):
-        self.Maze = Maze()
-        self.Player = Player()
-        self.App = App()
+        self._running = True
+        self._display_surf = None
+        self._image_surf = None
+        self._block_surf = None
+        self.player = Player()
+        self.maze = Maze()
+        
+ 
+    def on_init(self):
+       pygame.init()
+       self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+        
+       pygame.display.set_caption('maze game')
+       self._running = True
+       self._image_surf = pygame.image.load("pygame.png").convert()
+       self._block_surf = pygame.image.load("pygame.png").convert()
+ 
+    def on_event(self, event):
+        if event.type == QUIT:
+            self._running = False
+ 
+
+
+    def on_render(self):
+        self._display_surf.fill((0,0,0))
+        self._display_surf.blit(self._image_surf,(self.player.x,self.player.y))
+        self.maze.draw(self._display_surf, self._block_surf)
+        pygame.display.flip()
+
+    def on_cleanup(self):
+        pygame.quit()
+  
     
     def on_execute(self):
-        if self.App.on_init() == False:
-            self.App._running = False
+        if self.on_init() == False:
+            self._running = False
  
-        while( self.App._running ):
+        while( self._running ):
             
             pygame.event.pump()
             keys = pygame.key.get_pressed()
             
             if (keys[ord('a')]):
-                self.Player.moveRight()
+                self.player.moveRight()
  
             if (keys[ord('d')]):
-                self.Player.moveLeft()
+                self.player.moveLeft()
  
             if (keys[ord('w')]):
-                self.Player.moveUp()
+                self.player.moveUp()
  
             if (keys[ord('s')]):
-                self.Player.moveDown()
+                self.player.moveDown()
  
-            if (keys[K_ESCAPE]):
-                self.App._running = False
+            if (keys[ord('q')]):
+                self._running = False
+            
  
-            self.App.on_loop()
-            self.App.on_render()
-        self.App.on_cleanup()
+            self.on_render()
+        self.on_cleanup()
   
 if __name__ == '__main__':
     theApp = MazeController()
     theApp.on_execute()
-  
-
 
 
