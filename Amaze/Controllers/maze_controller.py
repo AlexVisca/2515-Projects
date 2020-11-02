2ff1b05c0ed95f39ced1782a9e3be9700f821c54
apu-pixel <62687887+apu-pixel@users.noreply.github.com>
2020-11-01 20:32:58 -0800

    Update maze_controller.py

--- Amaze/Controllers/maze_controller.py
+++ Amaze/Controllers/maze_controller.py
@@ -3,25 +3,50 @@ from pygame.locals import *
 import pygame
 from maze import Maze
 
+
 class MazeController:
-    pass
-
-
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self._image_surf = None
+        self._block_surf = None
+        self.player = Player()
+        self.maze = Maze()
+        self.view = View(self)
+  
+    def on_execute(self):
+        if self.view.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            
+            pygame.event.pump()
+            keys = pygame.key.get_pressed()
+            
+            if (keys[ord('a')]):
+                self.player.moveRight()
+ 
+            if (keys[ord('d')]):
+                self.player.moveLeft()
+ 
+            if (keys[ord('w')]):
+                self.player.moveUp()
+ 
+            if (keys[ord('s')]):
+                self.player.moveDown()
+ 
+            if (keys[ord('q')]):
+                self._running = False
+            
+ 
+            self.view.on_render()
+        self.view.on_cleanup()
+  
 if __name__ == '__main__':
-    running = True
-    while running:
-        if input not in ['a', 's', 'w', 'd']:
-            print("Use WASD to move player.")
-        elif input == 'a':
-            pass
-        elif input == 's':
-            pass
-        elif input == 'w':
-            pass
-        elif input == 'd':
-            pass
-        elif input == 'q':
-            raise SystemExit
+    theApp = MazeController()
+    theApp.on_execute()
+  
+
 
 
 
